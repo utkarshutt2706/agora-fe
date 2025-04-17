d919f32c6733665a8d5c37ece347a4db9579ba8e
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2025-04-17 22:27:26 +0530

    Fix child routing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
 function Home() {
   return (
     <>
       <p>Home</p>
+      <Outlet></Outlet>
     </>
   );
 }
--- src/pages/PrivateRoute/index.tsx
+++ src/pages/PrivateRoute/index.tsx
@@ -2,21 +2,14 @@ import { JSX } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CustomRoute, routes } from '../../routes/index';
 
-const renderRoute = (routes: CustomRoute[]) => {
+const renderRoute = (routes: CustomRoute[]): JSX.Element[] => {
   return routes.map((route): JSX.Element => {
     return (
-      <>
-        <Route
-          key={route.path}
-          path={route.path}
-          element={<route.Component />}
-        />
-        {route.children && route.children.length ? (
-          renderRoute(route.children)
-        ) : (
-          <></>
-        )}
-      </>
+      <Route key={route.path} path={route.path} element={<route.Component />}>
+        {route.children &&
+          route.children.length > 0 &&
+          renderRoute(route.children)}
+      </Route>
     );
   });
 };
