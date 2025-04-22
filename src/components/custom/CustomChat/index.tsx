6ceeb0d486eb9f1e6b3f3ff5ba50814aa530e22e
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2025-04-23 03:43:41 +0530

    Implement chat time

--- src/components/custom/CustomChat/index.tsx
+++ src/components/custom/CustomChat/index.tsx
@@ -6,12 +6,16 @@ import {
 } from '@/components/ui/card';
 import AuthContext from '@/contexts/AuthContext';
 import { ChatResponseDto } from '@/dto';
+import moment from 'moment';
 import { useContext } from 'react';
 import UserAvatar from '../UserAvatar';
 
 function CustomChat({ chat }: { chat: ChatResponseDto }) {
   const authData = useContext(AuthContext);
   const isOwnMessage = authData?.user._id === chat.authorId;
+  const isChatModified =
+    new Date(chat.updatedAt).getTime() > new Date(chat.createdAt).getTime();
+
   return (
     <>
       <div
@@ -23,7 +27,7 @@ function CustomChat({ chat }: { chat: ChatResponseDto }) {
         <UserAvatar userName={chat.authorName} />
         <Card
           key={chat._id}
-          className={`w-[250px] lg:w-6/10 md:w-[500px] sm:w-[350px] ${
+          className={`relative w-[250px] lg:w-6/10 md:w-[500px] sm:w-[350px] ${
             isOwnMessage ? 'bg-neutral-200 mr-4' : 'ml-4'
           }`}
         >
@@ -31,6 +35,11 @@ function CustomChat({ chat }: { chat: ChatResponseDto }) {
             <CardTitle>{chat.authorName}</CardTitle>
             <CardDescription>{chat.body}</CardDescription>
           </CardHeader>
+          <span className='absolute bottom-[10px] right-[10px] text-muted-foreground text-sm'>
+            {isChatModified
+              ? `Edited ${moment(new Date(chat.updatedAt)).format('hh:mm a')}`
+              : `${moment(new Date(chat.createdAt)).format('hh:mm a')}`}
+          </span>
         </Card>
       </div>
     </>
