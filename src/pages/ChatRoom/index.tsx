c1c0c0f21526fd2e0ab2ea1e60a51efe7e33c96c
Utkarsh Srivastava <utkarsh.srivastava@unthinkable.co>
2025-04-23 02:51:58 +0530

    Handle user typing event

--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -13,7 +13,10 @@ import { useParams } from 'react-router-dom';
 function ChatRoom() {
   const params = useParams();
   const [message, setMessage] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
+  const [usersTyping, setUsersTyping] = useState<string[]>([]);
   const [chats, setChats] = useState([] as ChatResponseDto[]);
+  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
   const bottomRef = useRef<HTMLDivElement | null>(null);
   const socket = useSocket();
 
@@ -30,6 +33,8 @@ function ChatRoom() {
     if (socket && params.roomId) {
       socket.emit('join_room', params.roomId);
       socket.on('receive_message', onMessageReceive);
+      socket.on('user_typing_start', onUserTypingStart);
+      socket.on('user_typing_end', onUserTypingEnd);
     }
     return () => {
       if (socket && params.roomId) {
@@ -38,6 +43,18 @@ function ChatRoom() {
       }
     };
   }, [socket, params.roomId]);
+
+  const onUserTypingStart = (name: string) => {
+    console.log(name);
+    setUsersTyping((prev) => {
+      if (!prev.includes(name)) return [...prev, name];
+      return prev;
+    });
+  };
+
+  const onUserTypingEnd = (name: string) => {
+    setUsersTyping((prev) => prev.filter((n) => n !== name));
+  };
 
   const onMessageReceive = (chat: ChatResponseDto) => {
     setChats((chats) => [...chats, chat]);
@@ -68,7 +85,8 @@ function ChatRoom() {
     socket?.off('message_error');
   };
 
-  const sendMessage = () => {
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (socket && params.roomId) {
       const chatBody: ChatRequestDto = {
         body: message,
@@ -82,10 +100,22 @@ function ChatRoom() {
   };
 
   const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMessage(e.target.value);
-    if (socket) {
-      socket?.emit('typing', params.roomId);
+    const input = e.target.value;
+    setMessage(input);
+
+    if (!socket || !params.roomId) return;
+
+    if (!isTyping) {
+      socket.emit('typing_start', params.roomId);
     }
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+
+    typingTimeoutRef.current = setTimeout(() => {
+      setIsTyping(false);
+      socket.emit('typing_end', params.roomId);
+    }, 500);
   };
 
   return (
@@ -101,7 +131,16 @@ function ChatRoom() {
           {/* Invisible anchor to scroll to */}
           <div ref={bottomRef} />
         </div>
-        <div className='flex w-full items-center space-x-2'>
+        <form
+          className='relative flex w-full items-center space-x-2'
+          onSubmit={sendMessage}
+        >
+          {usersTyping.length > 0 && (
+            <div className='absolute text-sm text-muted-foreground px-2 pb-2 top-[-20px]'>
+              {usersTyping.join(', ')} {usersTyping.length === 1 ? 'is' : 'are'}{' '}
+              typing...
+            </div>
+          )}
           <Input
             type='text'
             className='h-12'
@@ -111,12 +150,11 @@ function ChatRoom() {
           />
           <Button
             type='submit'
-            onClick={sendMessage}
             className='h-12 lg:px-10 md:px-8 sm:px-6 xs:px-4 cursor-pointer'
           >
             Send
           </Button>
-        </div>
+        </form>
       </div>
     </>
   );
